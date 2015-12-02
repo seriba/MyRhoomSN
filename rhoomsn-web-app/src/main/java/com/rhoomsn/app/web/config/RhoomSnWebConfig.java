b7aca2da4c7cf5926d6c2cfd0b1e7e9371488116
nafofana <fofana.naby@yahoo.fr>
2015-12-02 13:08:35 +0100

    résolution accès backend demande congé

--- rhoomsn-web-app/src/main/java/com/rhoomsn/app/web/config/RhoomSnWebConfig.java
+++ rhoomsn-web-app/src/main/java/com/rhoomsn/app/web/config/RhoomSnWebConfig.java
@@ -14,10 +14,12 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-
 
 import java.io.IOException;
 import javax.servlet.Filter;
@@ -26,10 +28,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
@@ -53,62 +51,62 @@ public class RhoomSnWebConfig {
 		return new JpaTransactionManager();
 	}
 
-	@Bean
-	public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
-		return new MySecurityConfigurer();
-	}
+//	@Bean
+//	public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+//		return new MySecurityConfigurer();
+//	}
 
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	public static class MySecurityConfigurer extends WebSecurityConfigurerAdapter {
-		
-		//Spring Security nous propose un basic authentication form acceptant uniquement user user
-		 @Override
-		 protected void configure(AuthenticationManagerBuilder builder) throws
-		 Exception {
-		 builder.inMemoryAuthentication()
-		 .withUser("user").password("user").roles("USER");
-		
-		 }
-		 
-//		@Override
-//		protected void configure(HttpSecurity http) throws Exception {
-//			//pour customiser le login page
-//			http.authorizeRequests().antMatchers("/dist/*","/home/**","/index.html","/login.html","/").permitAll()
-//					.anyRequest().authenticated()
-//			.and().csrf()
-//			.csrfTokenRepository(csrfTokenRepository()).and()
-//			.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
-//		}
+//	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//	public static class MySecurityConfigurer extends WebSecurityConfigurerAdapter {
 //		
-//		private Filter csrfHeaderFilter() {
-//			return new OncePerRequestFilter() {
-//				@Override
-//				protected void doFilterInternal(HttpServletRequest request,
-//						HttpServletResponse response, FilterChain filterChain)
-//						throws ServletException, IOException {
-//					CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
-//							.getName());
-//					if (csrf != null) {
-//						Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-//						String token = csrf.getToken();
-//						if (cookie == null || token != null
-//								&& !token.equals(cookie.getValue())) {
-//							cookie = new Cookie("XSRF-TOKEN", token);
-//							cookie.setPath("/home");
-//							response.addCookie(cookie);
-//						}
-//					}
-//					filterChain.doFilter(request, response);
-//				}
-//			};
-//		}
-
-//		private CsrfTokenRepository csrfTokenRepository() {
-//			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-//			repository.setHeaderName("X-XSRF-TOKEN");
-//			return repository;
-//		}
-
-	}
+//		//Spring Security nous propose un basic authentication form acceptant uniquement user user
+//		 @Override
+//		 protected void configure(AuthenticationManagerBuilder builder) throws
+//		 Exception {
+//		 builder.inMemoryAuthentication()
+//		 .withUser("user").password("user").roles("USER");
+//		
+//		 }
+//		 
+////		@Override
+////		protected void configure(HttpSecurity http) throws Exception {
+////			//pour customiser le login page
+////			http.authorizeRequests().antMatchers("/dist/*","/home/**","/index.html","/login.html","/").permitAll()
+////					.anyRequest().authenticated()
+////			.and().csrf()
+////			.csrfTokenRepository(csrfTokenRepository()).and()
+////			.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+////		}
+////		
+////		private Filter csrfHeaderFilter() {
+////			return new OncePerRequestFilter() {
+////				@Override
+////				protected void doFilterInternal(HttpServletRequest request,
+////						HttpServletResponse response, FilterChain filterChain)
+////						throws ServletException, IOException {
+////					CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+////							.getName());
+////					if (csrf != null) {
+////						Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+////						String token = csrf.getToken();
+////						if (cookie == null || token != null
+////								&& !token.equals(cookie.getValue())) {
+////							cookie = new Cookie("XSRF-TOKEN", token);
+////							cookie.setPath("/home");
+////							response.addCookie(cookie);
+////						}
+////					}
+////					filterChain.doFilter(request, response);
+////				}
+////			};
+////		}
+//
+////		private CsrfTokenRepository csrfTokenRepository() {
+////			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+////			repository.setHeaderName("X-XSRF-TOKEN");
+////			return repository;
+////		}
+//
+//	}
 
 }
